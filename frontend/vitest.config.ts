4f34e8346c422c746ad50e536f357e9e23058837
Your Name <your-email@example.com>
2026-02-11 18:20:42 -0800

    make vitest work

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: { exclude: ['fsevents'] },
+
   test: {
     browser: {
       enabled: true,
