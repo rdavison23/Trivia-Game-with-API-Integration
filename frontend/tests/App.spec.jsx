500c633ab1286d0f1a5a16e9eabe5a1baba9ee29
Your Name <your-email@example.com>
2026-02-11 18:34:36 -0800

    added test expectation

--- frontend/tests/App.spec.jsx
+++ frontend/tests/App.spec.jsx
@@ -4,4 +4,10 @@ import App from '../src/App';
 
 test('renders game', async () => {
   expect(true).toBe(true);
+  const { getByText, getByRole } = await render(<App name="Vitest" />);
+
+  await expect.element(getByText('Start Trivia Game')).toBeInTheDocument();
+  await expect.element(getByText('Start Game')).toBeInTheDocument();
+
+  //await getByRole('button', { name: 'Increment '}).click()
 });
