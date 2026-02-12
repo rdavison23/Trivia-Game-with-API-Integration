c4d1582fe42222f60d64c816750c63960f6b15b0
Your Name <your-email@example.com>
2026-02-11 17:43:29 -0800

    added first app spec

---
+++ frontend/tests/App.spec.jsx
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
+import App from '../src/App';
+
+test('renders game', async () => {
+  expect(true).toBe(true);
+});
