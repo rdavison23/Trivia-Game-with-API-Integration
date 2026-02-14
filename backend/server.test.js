39eb295a88965e7b3c172b9d836dd5d0b0e77a19
Your Name <your-email@example.com>
2026-02-13 16:27:06 -0800

    test(trivia): implement real test for GET /trivia/questions

--- backend/server.test.js
+++ backend/server.test.js
@@ -5,6 +5,6 @@ const requestWithSupertest = supertest(server);
 
 describe('Game Endpoints', () => {
   it('GET /trivia/questions should show questions', async () => {
-    expect(true).toBeFalsy();
+    return supertest(server).get('/trivia/questions').expect(200);
   });
 });
